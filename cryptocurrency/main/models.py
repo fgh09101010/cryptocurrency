f22d16b03c8902c5a28a2b6120c460e825d5d0be
fgh09101010 <fgh09101010@gmail.com>
2024-11-24 11:08:22 +0800

    edit models

--- cryptocurrency/main/models.py
+++ cryptocurrency/main/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 
-# Create your models here.
-# models.py
+class Coin(models.Model):
+    coinname = models.CharField(max_length=50)
+    abbreviation = models.CharField(max_length=10)  # 假設這是加密貨幣的簡稱
+
+    def __str__(self):
+        return self.coinname
 
 class BitcoinPrice(models.Model):
-    coinname = models.CharField(max_length=50, default="DefaultCoin")
+    coin = models.ForeignKey(Coin, on_delete=models.CASCADE)
     usd = models.FloatField()
     twd = models.FloatField()
     eur = models.FloatField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField()
 
     def __str__(self):
-        return f"BTC Price at {self.timestamp}"
+        return f"{self.coin.coinname} - {self.timestamp}"
