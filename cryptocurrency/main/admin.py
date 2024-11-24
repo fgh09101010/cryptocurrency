1bb66ea2f567562d0cdc0d88e1c5438a43c758c7
fgh09101010 <fgh09101010@gmail.com>
2024-11-24 12:52:04 +0800

    add admin

--- cryptocurrency/main/admin.py
+++ cryptocurrency/main/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Coin,BitcoinPrice
+from django.utils.html import format_html
+
+@admin.register(Coin)
+class CoinAdmin(admin.ModelAdmin):
+    list_display = ('coinname', 'abbreviation', 'logo_url',"show_logo")
+
+    def show_logo(self, obj):
+        return format_html('<img src="{}" style="height: 40px;"/>', obj.logo_url)
+
+
+@admin.register(BitcoinPrice)
+class BitcoinPriceAdmin(admin.ModelAdmin):
+    list_display = ('coin', 'usd', 'twd','eur','timestamp')
+
+    
+
