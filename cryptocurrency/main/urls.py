11b3ed12d14f3bc2152a35774866062c37343af6
Yicheng0093 <yicheng814071@gmail.com>
2024-11-24 13:07:34 +0800

    login

--- cryptocurrency/main/urls.py
+++ cryptocurrency/main/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),  # 將路徑連結到視圖
     path('crypto/', views.crypto_list, name='crypto_list'),  # 用於展示虛擬貨幣價格的列表頁面（假設已經設置）
     path('crypto/<int:pk>/', views.crypto_detail, name='crypto_detail'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
