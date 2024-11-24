402f7f8b36e750926faeedc1f700ccbd212710a1
glo2913 <a291328962@gmail.com>
2024-11-24 16:26:40 +0800

    coindata

---
+++ cryptocurrency/api/coindata/ttt.py
@@ -0,0 +1,9 @@
+import subprocess
+
+python_path = "d:/env/Scripts/python.exe"  # 虛擬環境的 Python 路徑
+
+subprocess.run([python_path, "d:/新增資料夾/cryptocurrency/api/4.py"], check=True)
+subprocess.run([python_path, "d:/新增資料夾/cryptocurrency/api/901.py"], check=True)
+subprocess.run([python_path, "d:/新增資料夾/cryptocurrency/api/1801.py"], check=True)
+
+print("所有腳本執行完畢！")
