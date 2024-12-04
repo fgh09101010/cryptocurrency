4552a1193380c5d686581025807b899a17709fd9
Yicheng0093 <yicheng814071@gmail.com>
2024-12-04 19:38:53 +0800

    修正user_profile.html頁面內部連結

--- cryptocurrency/main/views.py
+++ cryptocurrency/main/views.py
@@ -393,18 +393,18 @@ def update_password(request):
 
         if not check_password(current_password, user.password):
             messages.error(request, '目前密碼不正確。', extra_tags='password')
-            return redirect('/upload/')
+            return redirect('/user_profile/')
 
         if new_password != confirm_password:
             messages.error(request, '新密碼與確認密碼不一致。', extra_tags='password')
-            return redirect('/upload/')
+            return redirect('/user_profile/')
 
         user.set_password(new_password)
         user.save()
         update_session_auth_hash(request, user)
 
         messages.success(request, '密碼已成功修改。', extra_tags='password')
-        return redirect('/upload/')
+        return redirect('/user_profile/')
 
     return render(request, 'user_profile.html')
 
