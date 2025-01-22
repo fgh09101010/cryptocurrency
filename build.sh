e2ebf755b6da0458acf54a8000359ac657d7e952
fgh09101010 <fgh09101010@gmail.com>
2025-01-22 19:30:01 +0800

    19

--- build.sh
+++ build.sh
@@ -7,5 +7,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py flush --noinput
-python manage.py createsu  # new
 python manage.py loaddata ./data.json
