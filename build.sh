800f0f97076f3326f1293b90ed76233440113457
fgh09101010 <fgh09101010@gmail.com>
2025-01-22 19:52:48 +0800

    21

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate --no-input
 python manage.py flush --noinput
-python manage.py loaddata ./data.json
+#python manage.py loaddata ./data.json --no-input
