f4a8af1bd6af91608df5ffbab5624c4fd7e9d942
fgh09101010 <fgh09101010@gmail.com>
2025-01-22 16:22:52 +0800

    12

--- build.sh
+++ build.sh
@@ -7,11 +7,13 @@ apt-get update && apt-get install -y \
     libpq-dev && apt-get clean
 
 # 安裝 Python 依賴
-pip install --no-cache-dir -r requirements.txt
+pip install -r requirements.txt
 
 # 執行 Django 遷移命令
-python manage.py makemigrations
-python manage.py migrate --noinput
+python manage.py collectstatic --no-input
+python manage.py migrate
 
 # 啟動應用
 gunicorn --bind 0.0.0.0:8000 cryptocurrency.wsgi:application
+
+
