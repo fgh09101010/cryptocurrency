b4d261940ded3f1118560a9b2fd8e70c72e79505
fgh09101010 <fgh09101010@gmail.com>
2025-01-23 10:18:58 +0800

    38

--- build.sh
+++ build.sh
@@ -7,4 +7,10 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
 python manage.py flush --noinput
+
+# 創建超級用戶，設置密碼為 'admin'
+python manage.py shell <<EOF
+from django.contrib.auth.models import User
+User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+EOF
 #python manage.py loaddata ./data.json --no-input
